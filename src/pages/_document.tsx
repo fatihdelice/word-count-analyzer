eba4a801c1378605d285807ea8243c9f784cfca2
fatihdelice <delicex58@gmail.com>
2024-11-03 00:56:40 +0300

    feat: SEO improved

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>Word Count Analyzer</title>
-        <meta name="description" content="Word Count Analyzer" />
+        <meta name="description" content="Analyze the word count in your PDF and DOCX files with our powerful tool." />
+        <meta name="keywords" content="word count, PDF, DOCX, text analysis, document analysis, word frequency, document editor, text statistics, readability, language processing, text extraction, writing tools, online word counter, file analysis" />
+        <meta name="author" content="Fatih Delice" />
+        <meta property="og:title" content="Word Count Analyzer" />
+        <meta property="og:description" content="Analyze the word count in your PDF and DOCX files with our powerful tool." />
+        <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="antialiased">
